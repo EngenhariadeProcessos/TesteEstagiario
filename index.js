1121b9ed67283be09619b750ad7d40c451be75b1
Akatchin <thiago.jp.221@gmail.com>
2022-12-30 23:16:25 -0300

    Finished exercise

---
+++ index.js
@@ -0,0 +1,57 @@
+const corpo = document.querySelector(".corpo")
+const form = document.querySelector(".formulario")
+
+function novoElemento (tagName, className){
+    const elem = document.createElement(tagName)
+    elem.className = className
+
+    return elem
+}
+
+//Criando a frase de agradecimento
+function criaMensagem () {
+    this.elemento = novoElemento("div", "mensagem")
+    corpo.appendChild(this.elemento)
+    this.Mensagem = novoElemento("p", "texto")
+    this.Mensagem.innerHTML = "Obrigado por seu cadastro!"
+    this.elemento.appendChild(this.Mensagem)
+    
+}
+
+const Mensagem = new criaMensagem()
+
+function enviar() {
+
+this.nome = document.getElementById("nome").value
+this.email = document.getElementById("email").value
+this.telefone = document.getElementById("telefone").value
+
+
+
+this.valorForm = {}
+
+//Validação dos campos e adição dos dados em um objeto
+
+    if(this.valorForm.Nome == "", this.valorForm.Email == "", this.valorForm.Telefone == "") {
+        alert("Campos em brancos não são permitidos")
+        return
+    } else {
+        this.valorForm.Nome = nome
+        this.valorForm.Email = email
+        this.valorForm.Telefone = telefone
+    }
+
+//Escondendo o formulario e exibindo mensagem de agradecimento
+
+    form.classList.add("invisivel")
+    this.exibirMensagem = document.querySelector('.mensagem')
+    this.exibirMensagem.classList.add("exibir")
+    console.log(this.valorForm)
+}
+
+
+
+
+
+
+
