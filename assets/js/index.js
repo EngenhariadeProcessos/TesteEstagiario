a47638eca19f14da60f839b4d0d2b7c4c4c0c5cc
Marcela Rodriguez <marcelaaprodriguez@gmail.com>
2023-02-01 14:23:30 -0300

    incluindo o js no código

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,25 @@
-  
+var mensagem = document.querySelector(".mensagem");
+var formulario = document.querySelector(".formulario");
+
+console.log(mensagem.textContent);
+
+
+function validacaoFormulario(event) {
+
+    let nome = document.forms["formulario"]["nome"].value;
+    let email = document.forms["formulario"]["email"].value;
+    let telefone = document.forms["formulario"]["telefone"].value;
+
+    if (nome == "" || email == "" || telefone == "") {
+      mensagem.textContent = "Todos os campos precisam estar preenchidos";
+      return false;
+    } else {
+      mensagem.textContent = "Obrigado por seu cadastro!";
+      console.log(nome);
+      console.log(email);
+      console.log(telefone);
+      formulario.hidden = true;
+      
+    }
+    event.preventDefault();
+}
