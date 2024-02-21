6893af9e50669a42419bccf16b474a811a859ec4
vittorioaugusto <vi.aborges47@gmail.com>
2024-02-21 11:40:23 -0300

    update

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
 function EnviarDados() {
-    // Captura os elementos do formulário e da mensagem por meio de seus IDs.
-    const formulario = document.querySelector('form');
-    const mensagem = document.getElementById('mensagem');
-    const nomeInput = document.getElementById('nome');
-    const emailInput = document.getElementById('email');
-    const telefoneInput = document.getElementById('telefone');
+    
+    const formulario = document.querySelector('form')
+    const nomeInput = document.getElementById('nome')
+    const emailInput = document.getElementById('email')
+    const telefoneInput = document.getElementById('telefone')
   
-    // Obtém os valores dos campos de entrada.
-    const nome = nomeInput.value;
-    const email = emailInput.value;
-    const telefone = telefoneInput.value;
+    // Valores dos campos de entrada.
+    const nome = nomeInput.value
+    const email = emailInput.value
+    const telefone = telefoneInput.value
 
     // Cria um objeto com os dados do usuário.
     const dadosUsuario = {
         Nome: nome,
         Email: email,
         Telefone: telefone
-    };
+    }
 
-    // Exibe o objeto no console.
-    console.log(dadosUsuario);
-  
-    // Exibe a mensagem de boas-vindas.
-    mensagem.style.display = 'block';
-  
-    // Atualiza os elementos da mensagem com os dados do usuário.
-    mensagem.querySelector('p:nth-child(3)').textContent = 'Nome: ' + nome;
-    mensagem.querySelector('p:nth-child(4)').textContent = 'Email: ' + email;
-    mensagem.querySelector('p:nth-child(5)').textContent = 'Telefone: ' + telefone;
+    // Exibe os dados no console.
+    console.log(dadosUsuario)
   
     // Oculta o formulário.
-    formulario.style.display = 'none';
-  
+    formulario.style.display = 'none'
+
+    // Exibir mensagem
+    const mensagem = document.getElementById('message')
+    // Exibe a mensagem de boas-vindas.
+    mensagem.style.display = 'block'
+
     // Impede que o formulário seja enviado.
-    event.preventDefault();
+    event.preventDefault()
+
+    // Atualiza os elementos da mensagem com os dados do usuário.
+    mensagem.querySelector('p:nth-child(2)').textContent = "Nome: " + nome
+    mensagem.querySelector('p:nth-child(3)').textContent = "Email: " + email
+    mensagem.querySelector('p:nth-child(4)').textContent = "Telefone: " + telefone
+  
 }
