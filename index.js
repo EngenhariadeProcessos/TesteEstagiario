902bfa0f37d06ee69ee50868ef1dd4c421ffc90a
vittorioaugusto <vi.aborges47@gmail.com>
2024-02-21 12:01:12 -0300

    Finished Exercise

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ function EnviarDados() {
     const email = emailInput.value
     const telefone = telefoneInput.value
 
+    // Oculta o formulário.
+    formulario.style.display = 'none'
+
+    // Exibe a mensagem de dados do usuário
+    const mensagem = document.getElementById('message')
+
+    // Exibe a mensagem de boas-vindas.
+    mensagem.style.display = 'block'
+
+    // Impede que o formulário seja enviado.
+    event.preventDefault()
+
     // Cria um objeto com os dados do usuário.
     const dadosUsuario = {
         Nome: nome,
@@ -19,17 +31,6 @@ function EnviarDados() {
 
     // Exibe os dados no console.
     console.log(dadosUsuario)
-  
-    // Oculta o formulário.
-    formulario.style.display = 'none'
-
-    // Exibir mensagem
-    const mensagem = document.getElementById('message')
-    // Exibe a mensagem de boas-vindas.
-    mensagem.style.display = 'block'
-
-    // Impede que o formulário seja enviado.
-    event.preventDefault()
 
     // Atualiza os elementos da mensagem com os dados do usuário.
     mensagem.querySelector('p:nth-child(2)').textContent = "Nome: " + nome
